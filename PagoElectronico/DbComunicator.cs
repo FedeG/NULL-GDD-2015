1754ba597510f506501b0b618c26a733c0366e41
FedeG <federicog@exo.com.ar>
2015-05-18 12:22:47 -0300

    Conector con la base de datos

---
+++ PagoElectronico/DbComunicator.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace PagoElectronico{
+
+    public class DbComunicator{
+        private SqlConnection ConexionConBD;
+        private SqlCommand Consulta;
+        private SqlDataReader Lector;
+        private string DbName;
+
+        public void EjecutarQuery(string query){
+            this.ConectarConDB();
+            this.getQuery(query);
+            this.CerrarConexion();
+        }
+
+        public void ObtenerQuery(string query){
+            Consulta = new SqlCommand(query, this.ConexionConBD);
+            this.Lector = Consulta.ExecuteReader();
+        }
+
+        public void ConectarConDB(){
+            // Crear la conexión con la base de datos
+            string strConexión = "Data Source=.\\sqlexpress2005;Initial Catalog=" + this.DbName + ";Integrated Security=True";
+            this.ConexionConBD = new SqlConnection(strConexión);
+
+            // Abrir la base de datos
+            this.ConexionConBD.Open();
+        }
+
+        public void CerrarConexion(){
+            // Cerrar la conexión cuando ya no sea necesaria
+            if (Lector != null)
+                Lector.Close();
+            if (ConexionConBD != null)
+                ConexionConBD.Close();
+        }
+
+        public void ExecuteTransaction(string nombre, List<string> querys){
+            this.ConectarConDB();
+            SqlCommand command = this.ConexionConBD.CreateCommand();
+            SqlTransaction transaction;
+            transaction = this.ConexionConBD.BeginTransaction(nombre);
+            command.Connection = this.ConexionConBD;
+            command.Transaction = transaction;
+            try{
+                foreach (string query in querys){
+                    command.CommandText = query;
+                    command.ExecuteNonQuery();
+                }
+                transaction.Commit();
+            }
+            catch (Exception ex){
+                Console.WriteLine("Commit Exception Type: {0}", ex.GetType());
+                Console.WriteLine(" Message: {0}", ex.Message);
+                try{
+                    transaction.Rollback();
+                } catch (Exception ex2){
+                    Console.WriteLine("Rollback Exception Type: {0}", ex2.GetType());
+                    Console.WriteLine(" Message: {0}", ex2.Message);
+                }
+            }
+            this.CerrarConexion();
+        }
+
+        // Ejemplo de uso
+        public static void Main(){
+            DbComunicator db = new DbComunicator();
+            try {
+                db.EjecutarQuery("SELECT Nombre FROM CLIENT");
+            }
+            catch (Exception e){
+                Console.WriteLine("Error: " + e.Message);
+            }
+            finally {
+                Console.WriteLine("Los nombres de los clientes son: ");
+                while (db.Lector.Read()){
+                    Console.WriteLine(" - " + db.Lector["Nombre"]);
+                };
+            }
+        }
+
+    }
+
+}
