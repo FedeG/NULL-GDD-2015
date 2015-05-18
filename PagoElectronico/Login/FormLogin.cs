018cbc41908b3bc0ad41ff1afaca987545b31e4d
FedeG <federicog@exo.com.ar>
2015-05-18 15:21:58 -0300

    Ejemplo de uso en el login (borrar despues)

--- PagoElectronico/Login/FormLogin.cs
+++ PagoElectronico/Login/FormLogin.cs
@@ -9,16 +9,25 @@ using System.Windows.Forms;
 
 namespace PagoElectronico.Login
 {
-    public partial class FormLogin : Form
-    {
-        public FormLogin()
-        {
+    public partial class FormLogin : Form{
+        public FormLogin(){
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
+        private void button1_Click(object sender, EventArgs e){
             this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e){
+            DbComunicator db = new DbComunicator();
+            db.EjecutarQuery("SELECT TOP(100) * FROM gd_esquema.Maestra");
+            label2.Text = "";
+            while (db.getLector().Read()){
+                this.label2.Text += db.getLector()["Cli_Nombre"].ToString() +
+                    " "+ db.getLector()["Cli_Apellido"].ToString() +
+                    System.Environment.NewLine;
+            };
+            db.CerrarConexion();
         }
     }
 }
