b7a5acaaafcd51c3c19730afb0fba49ab7d9ffd4
Agustin Ignacio Guido Croce Villanustre <agustinc@exo.com.ar>
2015-05-30 21:25:24 -0300

    Se agrega * para enmascarar password. Se encripta el password en sha256. Se verificar la existencia del user:password en la base.

--- PagoElectronico/Login/FormLogin.cs
+++ PagoElectronico/Login/FormLogin.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Security.Cryptography;
 
 namespace PagoElectronico.Login
 {
@@ -18,14 +19,19 @@ namespace PagoElectronico.Login
             this.Close();
         }
 
+        public String getHashString(String password){
+            byte[] passwordSha256 = new SHA256Managed().ComputeHash(Encoding.UTF8.GetBytes(password));
+            string result = BitConverter.ToString(passwordSha256)
+                .Replace("-", string.Empty)
+                .ToLower();
+            return result;
+        }
+
         private void button2_Click(object sender, EventArgs e){
             DbComunicator db = new DbComunicator();
-            db.EjecutarQuery("SELECT TOP(100) * FROM gd_esquema.Maestra");
-            label2.Text = "";
+            db.EjecutarQuery("SELECT * FROM [GD1C2015].[NULL].[USUARIO] WHERE Usr_Username = '" + textBox1.Text + "' and Usr_Password = '" + this.getHashString(textBox2.Text) + "'");
             while (db.getLector().Read()){
-                this.label2.Text += db.getLector()["Cli_Nombre"].ToString() +
-                    " "+ db.getLector()["Cli_Apellido"].ToString() +
-                    System.Environment.NewLine;
+                MessageBox.Show(db.getLector()["Usr_Username"].ToString() + " ha sido logeado");
             };
             db.CerrarConexion();
         }
