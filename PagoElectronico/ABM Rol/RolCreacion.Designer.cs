009cd6f4e4b6bd2c3d0d78acb8edd65a126bcf6c
Agustin Ignacio Guido Croce Villanustre <agustinc@exo.com.ar>
2015-06-05 09:50:11 -0300

    Pantalla creacion de rol.

---
+++ PagoElectronico/ABM Rol/RolCreacion.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace PagoElectronico.ABM_Rol
+{
+    partial class RolCreacion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.rolNameBox = new System.Windows.Forms.TextBox();
+            this.funcionalidadesListBox = new System.Windows.Forms.CheckedListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboEstado = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre del rol";
+            // 
+            // rolNameBox
+            // 
+            this.rolNameBox.Location = new System.Drawing.Point(109, 10);
+            this.rolNameBox.Name = "rolNameBox";
+            this.rolNameBox.Size = new System.Drawing.Size(100, 20);
+            this.rolNameBox.TabIndex = 1;
+            // 
+            // funcionalidadesListBox
+            // 
+            this.funcionalidadesListBox.FormattingEnabled = true;
+            this.funcionalidadesListBox.Location = new System.Drawing.Point(49, 121);
+            this.funcionalidadesListBox.Name = "funcionalidadesListBox";
+            this.funcionalidadesListBox.Size = new System.Drawing.Size(120, 94);
+            this.funcionalidadesListBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Estado";
+            // 
+            // comboEstado
+            // 
+            this.comboEstado.FormattingEnabled = true;
+            this.comboEstado.Location = new System.Drawing.Point(109, 45);
+            this.comboEstado.Name = "comboEstado";
+            this.comboEstado.Size = new System.Drawing.Size(100, 21);
+            this.comboEstado.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(68, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Funcionalidades";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(118, 230);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 31);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Crear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ABMDeRol
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(227, 273);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboEstado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.funcionalidadesListBox);
+            this.Controls.Add(this.rolNameBox);
+            this.Controls.Add(this.label1);
+            this.Name = "ABMDeRol";
+            this.Text = "ABM de Rol";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox rolNameBox;
+        private System.Windows.Forms.CheckedListBox funcionalidadesListBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboEstado;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ PagoElectronico/ABM Rol/RolCreacion.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace PagoElectronico.ABM_Rol
+{
+    public partial class RolCreacion : Form
+    {
+        DbComunicator db;
+        public RolCreacion()
+        {
+            InitializeComponent();
+            comboEstado.Items.Add("Habilitado");
+            comboEstado.Items.Add("Deshabilitado");
+
+            this.db = new DbComunicator();
+            this.db.EjecutarQuery("SELECT Func_Cod, Func_Nombre FROM [GD1C2015].[NULL].[Funcionalidad]");
+
+            while (this.db.getLector().Read()) {
+                string nombre = this.db.getLector()["Func_Nombre"].ToString();
+                int cod = Convert.ToInt16(this.db.getLector()["Func_Cod"].ToString()) - 1;
+                funcionalidadesListBox.Items.Insert(cod ,this.db.getLector()["Func_Nombre"]);    
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataTable funcionalidadesDelRol = new DataTable();
+            funcionalidadesDelRol.Columns.Add("number", typeof(int));
+
+            foreach (int checkedIndex in funcionalidadesListBox.CheckedIndices)
+            {
+                funcionalidadesDelRol.Rows.Add(checkedIndex + 1);
+            }
+
+            SqlCommand spCrearRol = this.db.GetStoreProcedure("NULL.spCrearRol");
+            spCrearRol.Parameters.Add(new SqlParameter("@Rol_Nombre", rolNameBox.Text));
+            spCrearRol.Parameters.Add(new SqlParameter("@Rol_Estado", comboEstado.SelectedItem.ToString()));
+            spCrearRol.Parameters.Add(new SqlParameter("@Lista_Funcionalidades", funcionalidadesDelRol));
+            spCrearRol.ExecuteNonQuery();
+        }
+
+    }
+}
