439b1af685754dd1c288bf836f9cdbf353580fd9
Agustin Ignacio Guido Croce Villanustre <agustinc@exo.com.ar>
2015-06-04 21:38:16 -0300

    Pantalla Base de deposito con select tipo de moneda.

---
+++ PagoElectronico/Depositos/FormDeposito.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PagoElectronico.Depositos
+{
+    public partial class FormDeposito : Form
+    {
+        public FormDeposito()
+        {
+            InitializeComponent();
+            DbComunicator db = new DbComunicator();
+            db.EjecutarQuery("SELECT Moneda_Nombre, Moneda_Simbolo FROM [GD1C2015].[NULL].[Moneda]");
+            while (db.getLector().Read())
+            {
+                comboMoneda.Items.Add(db.getLector()["Moneda_Nombre"].ToString());
+            }
+        }
+
+        private void botonRealizar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Se realizo");
+        }
+
+
+    }
+}
