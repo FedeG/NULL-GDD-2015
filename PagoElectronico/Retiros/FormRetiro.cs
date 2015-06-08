a661f20ab3cce1cba2b139cb04a80992739e279d
Agustin Ignacio Guido Croce Villanustre <agustinc@exo.com.ar>
2015-06-08 10:42:14 -0300

    Se agrega llamado a store procedure y validaciones.

--- PagoElectronico/Retiros/FormRetiro.cs
+++ PagoElectronico/Retiros/FormRetiro.cs
@@ -12,6 +12,7 @@ namespace PagoElectronico.Retiros
 {
     public partial class FormRetiro : Form
     {
+        string username;
         public FormRetiro()
         {
             InitializeComponent();
@@ -20,13 +21,40 @@ namespace PagoElectronico.Retiros
         private void realizarButton_Click(object sender, EventArgs e)
         {
             DbComunicator db = new DbComunicator();
-            SqlCommand spCrearRol = db.GetStoreProcedure("NULL.spRealizarRetiro");
-            spCrearRol.Parameters.Add(new SqlParameter("@Rol_Nombre", rolNameBox.Text));
-            spCrearRol.Parameters.Add(new SqlParameter("@Rol_Estado", comboEstado.SelectedItem.ToString()));
-            spCrearRol.Parameters.Add(new SqlParameter("@Lista_Funcionalidades", funcionalidadesDelRol));
-            spCrearRol.ExecuteNonQuery();
+            SqlCommand spRealizarRetiro = db.GetStoreProcedure("NULL.spRealizarRetiro");
+            SqlParameter returnParameter = spRealizarRetiro.Parameters.Add("RetVal", SqlDbType.Int);
+            returnParameter.Direction = ParameterDirection.ReturnValue;
+            spRealizarRetiro.Parameters.Add(new SqlParameter("@Username", username));
+            spRealizarRetiro.Parameters.Add(new SqlParameter("@TipoDoc_Cod", tipoDocComboBox.SelectedText));
+            spRealizarRetiro.Parameters.Add(new SqlParameter("@Nro_Doc", nroDocTextBox.Text));
+            spRealizarRetiro.Parameters.Add(new SqlParameter("@Cuenta_Numero", cuentaComboBox.SelectedText));
+            spRealizarRetiro.Parameters.Add(new SqlParameter("@Importe", importeTextBox.Text));
+            //Agregar la fecha de sistema.
+            spRealizarRetiro.ExecuteNonQuery();
 
-            this.Close();
+            if ((int)returnParameter.Value == 0) {
+                this.Close();
+            }
+
+            if ((int)returnParameter.Value == 1)
+            {
+                MessageBox.Show("El Tipo y/o Numero de Documento no coinciden con los del usuario logeado.");
+            }
+            
+            if ((int)returnParameter.Value == 2)
+            {
+                MessageBox.Show("La Cuenta seleccionada no tiene saldo.");
+            }
+            
+            if ((int)returnParameter.Value == 3)
+            {
+                MessageBox.Show("El saldo disponible es insuficiente para realizar el retiro.");
+            }
+
+            if ((int)returnParameter.Value == 4)
+            {
+                MessageBox.Show("La cuenta no se encuentra Habilitada.");
+            }
         }
 
     }
