6e74b1a610d36f1b7a5f7ae95890a235242d571f
FedeG <federicog@exo.com.ar>
2015-06-08 15:28:29 -0300

    #8 - Pantalla de Listado de clientes y busqueda

---
+++ PagoElectronico/ABM Cliente/ClienteListado.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace PagoElectronico.ABM_Cliente
+{
+    partial class ClienteListado
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clienteTable = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ClienteUsername = new System.Windows.Forms.TextBox();
+            this.createClienteButton = new System.Windows.Forms.Button();
+            this.searchUsernameButton = new System.Windows.Forms.Button();
+            this.editarClienteButton = new System.Windows.Forms.Button();
+            this.habilitarButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.TipoDocCliente = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DocCliente = new System.Windows.Forms.TextBox();
+            this.searchDocumentoButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.clienteTable)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // clienteTable
+            // 
+            this.clienteTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.clienteTable.Location = new System.Drawing.Point(12, 119);
+            this.clienteTable.Name = "clienteTable";
+            this.clienteTable.ReadOnly = true;
+            this.clienteTable.Size = new System.Drawing.Size(738, 150);
+            this.clienteTable.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username";
+            // 
+            // ClienteUsername
+            // 
+            this.ClienteUsername.Location = new System.Drawing.Point(67, 26);
+            this.ClienteUsername.Name = "ClienteUsername";
+            this.ClienteUsername.Size = new System.Drawing.Size(214, 20);
+            this.ClienteUsername.TabIndex = 2;
+            // 
+            // createClienteButton
+            // 
+            this.createClienteButton.Location = new System.Drawing.Point(13, 293);
+            this.createClienteButton.Name = "createClienteButton";
+            this.createClienteButton.Size = new System.Drawing.Size(75, 23);
+            this.createClienteButton.TabIndex = 3;
+            this.createClienteButton.Text = "Crear Ciente";
+            this.createClienteButton.UseVisualStyleBackColor = true;
+            this.createClienteButton.Click += new System.EventHandler(this.createClienteButton_Click);
+            // 
+            // searchUsernameButton
+            // 
+            this.searchUsernameButton.Location = new System.Drawing.Point(206, 61);
+            this.searchUsernameButton.Name = "searchUsernameButton";
+            this.searchUsernameButton.Size = new System.Drawing.Size(75, 23);
+            this.searchUsernameButton.TabIndex = 4;
+            this.searchUsernameButton.Text = "Buscar";
+            this.searchUsernameButton.UseVisualStyleBackColor = true;
+            this.searchUsernameButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // editarClienteButton
+            // 
+            this.editarClienteButton.Location = new System.Drawing.Point(336, 293);
+            this.editarClienteButton.Name = "editarClienteButton";
+            this.editarClienteButton.Size = new System.Drawing.Size(75, 23);
+            this.editarClienteButton.TabIndex = 5;
+            this.editarClienteButton.Text = "Editar";
+            this.editarClienteButton.UseVisualStyleBackColor = true;
+            this.editarClienteButton.Click += new System.EventHandler(this.editarClienteButton_Click);
+            // 
+            // habilitarButton
+            // 
+            this.habilitarButton.Location = new System.Drawing.Point(675, 293);
+            this.habilitarButton.Name = "habilitarButton";
+            this.habilitarButton.Size = new System.Drawing.Size(75, 23);
+            this.habilitarButton.TabIndex = 6;
+            this.habilitarButton.Text = "Habilitar";
+            this.habilitarButton.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.ClienteUsername);
+            this.groupBox1.Controls.Add(this.searchUsernameButton);
+            this.groupBox1.Location = new System.Drawing.Point(13, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(299, 100);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Buscar por Username";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.TipoDocCliente);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.DocCliente);
+            this.groupBox2.Controls.Add(this.searchDocumentoButton);
+            this.groupBox2.Location = new System.Drawing.Point(318, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(432, 100);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Buscar por Documento";
+            // 
+            // TipoDocCliente
+            // 
+            this.TipoDocCliente.FormattingEnabled = true;
+            this.TipoDocCliente.Location = new System.Drawing.Point(44, 26);
+            this.TipoDocCliente.Name = "TipoDocCliente";
+            this.TipoDocCliente.Size = new System.Drawing.Size(101, 21);
+            this.TipoDocCliente.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Tipo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(151, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Número";
+            // 
+            // DocCliente
+            // 
+            this.DocCliente.Location = new System.Drawing.Point(201, 26);
+            this.DocCliente.Name = "DocCliente";
+            this.DocCliente.Size = new System.Drawing.Size(214, 20);
+            this.DocCliente.TabIndex = 2;
+            // 
+            // searchDocumentoButton
+            // 
+            this.searchDocumentoButton.Location = new System.Drawing.Point(340, 61);
+            this.searchDocumentoButton.Name = "searchDocumentoButton";
+            this.searchDocumentoButton.Size = new System.Drawing.Size(75, 23);
+            this.searchDocumentoButton.TabIndex = 4;
+            this.searchDocumentoButton.Text = "Buscar";
+            this.searchDocumentoButton.UseVisualStyleBackColor = true;
+            this.searchDocumentoButton.Click += new System.EventHandler(this.searchDocumentoButton_Click);
+            // 
+            // ClienteListado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(762, 336);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.habilitarButton);
+            this.Controls.Add(this.editarClienteButton);
+            this.Controls.Add(this.createClienteButton);
+            this.Controls.Add(this.clienteTable);
+            this.Name = "ClienteListado";
+            this.Text = "Listado Clientes";
+            ((System.ComponentModel.ISupportInitialize)(this.clienteTable)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView clienteTable;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ClienteUsername;
+        private System.Windows.Forms.Button createClienteButton;
+        private System.Windows.Forms.Button searchUsernameButton;
+        private System.Windows.Forms.Button editarClienteButton;
+        private System.Windows.Forms.Button habilitarButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox DocCliente;
+        private System.Windows.Forms.Button searchDocumentoButton;
+        private System.Windows.Forms.ComboBox TipoDocCliente;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ PagoElectronico/ABM Cliente/ClienteListado.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace PagoElectronico.ABM_Cliente
+{
+    public partial class ClienteListado : Form
+    {
+        DbComunicator db;
+        public ClienteListado()
+        {
+            InitializeComponent();
+            db = new DbComunicator();
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            this.SearchClientePorUsername();
+        }
+
+        void SearchClientePorUsername() {
+            clienteTable.DataSource = db.GetDataAdapter("SELECT * FROM [GD1C2015].[NULL].[Cliente] WHERE Cli_Nombre LIKE '%" + ClienteUsername.Text + "%'").Tables[0];
+        }
+
+        void SearchClientePorDocumento()
+        {
+            clienteTable.DataSource = db.GetDataAdapter("SELECT * FROM [GD1C2015].[NULL].[Cliente] WHERE Cli_Nro_Doc LIKE '%" + DocCliente.Text + "%' AND TipoDoc_Cod LIKE '%" + TipoDocCliente.Text + "%'").Tables[0];
+        }
+
+        void SearchClientePorUsername(object sender, FormClosedEventArgs e)
+        {
+            this.SearchClientePorUsername();
+        }
+
+        private void searchDocumentoButton_Click(object sender, EventArgs e)
+        {
+            this.SearchClientePorDocumento();
+        }
+
+        private void editarClienteButton_Click(object sender, EventArgs e)
+        {
+            ClienteEdicion re = new ClienteEdicion(clienteTable.SelectedRows[0]);
+            re.FormClosed += new FormClosedEventHandler(SearchClientePorUsername);
+            re.Show();
+        }
+
+        private void createClienteButton_Click(object sender, EventArgs e)
+        {
+            new ClienteCreacion().Show();
+        }
+        
+    }
+}
