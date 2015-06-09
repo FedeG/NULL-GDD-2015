7ec6a3f3f86e75582da0165b3816cf42169cd990
FedeG <federicog@exo.com.ar>
2015-06-09 03:33:48 -0300

    #8 - SP ya testeados

--- stored_procedure_abm_cliente.sql
+++ stored_procedure_abm_cliente.sql
@@ -1,5 +1,5 @@
 IF OBJECT_ID (N'NULL.spCrearCliente') IS NOT NULL
-   DROP FUNCTION "NULL".spCrearCliente
+   DROP PROCEDURE "NULL".spCrearCliente
 GO
 
 CREATE PROCEDURE "NULL".spCrearCliente
@@ -30,7 +30,7 @@ BEGIN
 	SET NOCOUNT ON;
 
   INSERT INTO [GD1C2015].[NULL].[Usuario] (Usr_Username, Usr_Password, Usr_Fecha_Creacion, Usr_Fecha_Ultima_Modificacion, Usr_Pregunta_Secreta, Usr_Respuesta_Secreta, Usr_Intentos_Login, Usr_Borrado, Usr_Estado)
-	VALUES (@Usr_Username, @Usr_Password, GETDATE(), GETDATE(), @Usr_Pregunta_Secreta, @Usr_Respuesta_Secreta, 0, 0, "Habilitado")
+	VALUES (@Usr_Username, @Usr_Password, GETDATE(), GETDATE(), @Usr_Pregunta_Secreta, @Usr_Respuesta_Secreta, 0, 0, 'Habilitado')
 
 	INSERT INTO [GD1C2015].[NULL].[Cliente] (Usr_Username, Cli_Nombre, Cli_Apellido, TipoDoc_Cod, Cli_Nro_Doc, Cli_Dom_Calle, Cli_Dom_Nro, Cli_Dom_Piso, Cli_Dom_Depto, Cli_Localidad, Cli_Fecha_Nac, Cli_Mail, Cli_Nacionalidad, Pais_Codigo, Cli_Borrado)
 	VALUES (@Usr_Username, @Cli_Nombre, @Cli_Apellido, @TipoDoc_Cod, @Cli_Nro_Doc, @Cli_Dom_Calle, @Cli_Dom_Nro, @Cli_Dom_Piso, @Cli_Dom_Depto, @Cli_Localidad, @Cli_Fecha_Nac, @Cli_Mail, @Cli_Nacionalidad, @Pais_Codigo, 0)
@@ -39,7 +39,7 @@ END
 GO
 
 IF OBJECT_ID (N'NULL.spEditarCliente') IS NOT NULL
-   DROP FUNCTION "NULL".spEditarCliente
+   DROP PROCEDURE "NULL".spEditarCliente
 GO
 
 CREATE PROCEDURE "NULL".spEditarCliente
@@ -81,7 +81,7 @@ END
 GO
 
 IF OBJECT_ID (N'NULL.spHabilitarUsuario') IS NOT NULL
-   DROP FUNCTION "NULL".spHabilitarUsuario
+   DROP PROCEDURE "NULL".spHabilitarUsuario
 GO
 
 CREATE PROCEDURE "NULL".spHabilitarUsuario
@@ -98,7 +98,7 @@ END
 GO
 
 IF OBJECT_ID (N'NULL.spDarDeBajaCliente') IS NOT NULL
-   DROP FUNCTION "NULL".spDarDeBajaCliente
+   DROP PROCEDURE "NULL".spDarDeBajaCliente
 GO
 
 CREATE PROCEDURE "NULL".spDarDeBajaCliente
