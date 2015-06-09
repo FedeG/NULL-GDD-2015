918ab94f78b2e7797525d1bfc79b42eb53ff2f4b
FedeG <federicog@exo.com.ar>
2015-06-09 01:46:35 -0300

    #8 - Data inicial de usuario y cliente para edición, carga de opciones para nacionalida y tipo de documento y boton de cancelar

--- PagoElectronico/ABM Cliente/ClienteEdicion.cs
+++ PagoElectronico/ABM Cliente/ClienteEdicion.cs
@@ -12,9 +12,54 @@ namespace PagoElectronico.ABM_Cliente
 {
     public partial class ClienteEdicion : ClienteData
     {
+        string username;
+        Dictionary<object, object> TipoDocs;
 
-        public ClienteEdicion(DataGridViewRow selected){
+        public ClienteEdicion(DataGridViewRow selected)
+        {
             InitializeComponent();
+            this.db = new DbComunicator();
+            this.getDictionarys();
+            this.username = selected.Cells["Usr_Username"].Value.ToString();
+            this.LoadUserData();
+            this.LoadClientData(selected);
+        }
+
+        private void LoadClientData(DataGridViewRow selected){
+            Nombre.Text = selected.Cells["Cli_Nombre"].Value.ToString();
+            Apellido.Text = selected.Cells["Cli_Apellido"].Value.ToString();
+            NacCliente.Text = selected.Cells["Cli_Nacionalidad"].Value.ToString();
+            Calle.Text = selected.Cells["Cli_Dom_Calle"].Value.ToString();
+            NumDomicilio.Text = selected.Cells["Cli_Dom_Nro"].Value.ToString();
+            db.EjecutarQuery("SELECT * FROM [GD1C2015].[NULL].[Cliente] WHERE Usr_Username = '" + username + "'");
+            while (db.getLector().Read()) {
+                Mail.Text = db.getLector()["Cli_Mail"].ToString();
+                TipoDocCliente.Text = this.TipoDocs[db.getLector()["TipoDoc_Cod"]].ToString();
+                NumDoc.Text = db.getLector()["Cli_Nro_Doc"].ToString();
+                Piso.Text = db.getLector()["Cli_Dom_Piso"].ToString();
+                Depto.Text = db.getLector()["Cli_Dom_Depto"].ToString();
+                Localidad.Text = db.getLector()["Cli_Localidad"].ToString();
+                FechaNacimiento.Value = Convert.ToDateTime(db.getLector()["Cli_Fecha_Nac"].ToString());
+            }
+        }
+
+        private void LoadUserData(){
+            Username.Text = this.username;
+            db.EjecutarQuery("SELECT Usr_Pregunta_Secreta FROM [GD1C2015].[NULL].[Usuario] WHERE Usr_Username = '" + username + "'");
+            while (db.getLector().Read()){
+                Pregunta.Text = db.getLector()["Usr_Pregunta_Secreta"].ToString();
+            }
+        }
+        
+        private void getDictionarys(){
+            this.db.ConectarConDB();
+            this.TipoDocs = db.GetQueryDictionary("SELECT TipoDoc_Cod, TipoDoc_Desc FROM GD1C2015.[NULL].TipoDoc WHERE TipoDoc_Borrado=0", "TipoDoc_Cod", "TipoDoc_Desc");
+            this.db.CerrarConexion();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
 
     }
