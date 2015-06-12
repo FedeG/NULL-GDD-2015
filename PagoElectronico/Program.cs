d1cec5752c7c8c5445bfd1c034e87cb1b0b7e59d
Agustin Ignacio Guido Croce Villanustre <agustinc@exo.com.ar>
2015-06-12 00:21:27 -0300

    Merge con rama depositos

--- PagoElectronico/Program.cs
+++ PagoElectronico/Program.cs
@@ -12,7 +12,7 @@ namespace PagoElectronico
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Depositos.FormDeposito());
+            Application.Run(new Depositos.DepositoForm());
         }
     }
 }
