fbde9b3ff5950cd69b832efbeb53f74c80feb47e
FedeG <federicog@exo.com.ar>
2015-06-15 11:04:05 -0300

    Formulario de Facturacion

---
+++ PagoElectronico/Facturacion/FormFacturacion.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace PagoElectronico.Facturacion
+{
+    partial class FormFacturacion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clienteTable = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnFacturar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.clienteTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // clienteTable
+            // 
+            this.clienteTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.clienteTable.Location = new System.Drawing.Point(12, 25);
+            this.clienteTable.Name = "clienteTable";
+            this.clienteTable.ReadOnly = true;
+            this.clienteTable.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.clienteTable.Size = new System.Drawing.Size(709, 237);
+            this.clienteTable.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Items pendientes de pago";
+            // 
+            // btnFacturar
+            // 
+            this.btnFacturar.Location = new System.Drawing.Point(612, 272);
+            this.btnFacturar.Name = "btnFacturar";
+            this.btnFacturar.Size = new System.Drawing.Size(109, 31);
+            this.btnFacturar.TabIndex = 3;
+            this.btnFacturar.Text = "Facturar";
+            this.btnFacturar.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(12, 272);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(109, 31);
+            this.btnSalir.TabIndex = 4;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // FormFacturacion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(733, 310);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnFacturar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.clienteTable);
+            this.Name = "FormFacturacion";
+            this.Text = "Facturación";
+            ((System.ComponentModel.ISupportInitialize)(this.clienteTable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView clienteTable;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnFacturar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
---
+++ PagoElectronico/Facturacion/FormFacturacion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PagoElectronico.Facturacion
+{
+    public partial class FormFacturacion : Form
+    {
+        DbComunicator db;
+
+        public FormFacturacion(string username){
+            InitializeComponent();
+            this.db = new DbComunicator();
+            
+            string queryItemsAPagar = "SELECT Cliente.Usr_Username, Cli_Nombre, Cli_Apellido, Cli_Nacionalidad, Cli_Dom_Calle, Cli_Dom_Nro, Cli_Borrado, Usr_Estado FROM (SELECT Usr_Username, Cli_Nombre, Cli_Apellido, Cli_Nacionalidad, Cli_Dom_Calle, Cli_Dom_Nro, Cli_Borrado FROM GD1C2015.[NULL].Cliente WHERE Usr_Username LIKE '%" + ClienteUsername.Text + "%') AS Cliente INNER JOIN GD1C2015.[NULL].Usuario AS Usuario ON Cliente.Usr_Username=Usuario.Usr_Username";
+            clienteTable.DataSource = db.GetDataAdapter(queryItemsAPagar).Tables[0];
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e){
+            this.Close();
+        }
+    }
+}
