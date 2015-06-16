f859ce934edcce6ae325f9cddfd01049e3518fba
Agustin Ignacio Guido Croce Villanustre <agustinc@exo.com.ar>
2015-06-16 09:43:57 -0300

    Se agrega from de tarjetas al menu principal.

--- PagoElectronico/Facturacion/FormFacturacion.cs
+++ PagoElectronico/Facturacion/FormFacturacion.cs
@@ -17,7 +17,7 @@ namespace PagoElectronico.Facturacion
             InitializeComponent();
             this.db = new DbComunicator();
             
-            string queryItemsAPagar = "SELECT Cliente.Usr_Username, Cli_Nombre, Cli_Apellido, Cli_Nacionalidad, Cli_Dom_Calle, Cli_Dom_Nro, Cli_Borrado, Usr_Estado FROM (SELECT Usr_Username, Cli_Nombre, Cli_Apellido, Cli_Nacionalidad, Cli_Dom_Calle, Cli_Dom_Nro, Cli_Borrado FROM GD1C2015.[NULL].Cliente WHERE Usr_Username LIKE '%" + ClienteUsername.Text + "%') AS Cliente INNER JOIN GD1C2015.[NULL].Usuario AS Usuario ON Cliente.Usr_Username=Usuario.Usr_Username";
+            string queryItemsAPagar = "SELECT Cliente.Usr_Username, Cli_Nombre, Cli_Apellido, Cli_Nacionalidad, Cli_Dom_Calle, Cli_Dom_Nro, Cli_Borrado, Usr_Estado FROM (SELECT Usr_Username, Cli_Nombre, Cli_Apellido, Cli_Nacionalidad, Cli_Dom_Calle, Cli_Dom_Nro, Cli_Borrado FROM GD1C2015.[NULL].Cliente WHERE Usr_Username LIKE '%" + username + "%') AS Cliente INNER JOIN GD1C2015.[NULL].Usuario AS Usuario ON Cliente.Usr_Username=Usuario.Usr_Username";
             clienteTable.DataSource = db.GetDataAdapter(queryItemsAPagar).Tables[0];
         }
 
