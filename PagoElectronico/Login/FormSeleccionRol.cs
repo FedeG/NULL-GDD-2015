197872919b535cd8c4a4fdc6f067b8002c4a439f
FedeG <federicog@exo.com.ar>
2015-07-21 17:46:19 -0300

    Uso del GetQueryDictionary en el seleccion de rol y manejo para cuando no tenes roles

--- PagoElectronico/Login/FormSeleccionRol.cs
+++ PagoElectronico/Login/FormSeleccionRol.cs
@@ -11,23 +11,28 @@ namespace PagoElectronico.Login
 {
     public partial class FormSeleccionDeRol : Form
     {
-        public string rolSeleccionado;
+        public string rolSeleccionado, username;
 
-        public FormSeleccionDeRol(DbComunicator db)
+        public FormSeleccionDeRol(string username)
         {
             InitializeComponent();
-            Dictionary<object, object> itemsComboBox = new Dictionary<object, object>();
-            while (db.getLector().Read())
-            {
-                itemsComboBox.Add(db.getLector()["Rol_Nombre"], db.getLector()["Rol_Nombre"]);
-            }
-            comboBox1.DataSource = new BindingSource(itemsComboBox, null);
-            comboBox1.DisplayMember = "Value";
-            comboBox1.ValueMember = "Key";
+            this.username = username;
+            string  queryRol = "SELECT r.Rol_Nombre FROM [GD1C2015].[NULL].[Rol_Usuario] AS ru, [GD1C2015].[NULL].[Rol] AS r " + 
+                "WHERE ru.Usr_Username = '" + username + "' AND ru.Rol_Nombre = r.Rol_Nombre AND r.Rol_Estado = 'Habilitado'";
+            DbComunicator db = new DbComunicator();
+            comboBox1.DataSource = new BindingSource(db.GetQueryDictionary(queryRol, "Rol_Nombre", "Rol_Nombre"), null);
+            comboBox1.DisplayMember = "Key";
+            comboBox1.ValueMember = "Value";
         }
 
         private void button1_Click(object sender, EventArgs e){
+            if (this.comboBox1.SelectedValue.ToString() == "No hay elementos para listar"){
+                MessageBox.Show("No tiene ningun rol habilitado, contactese con un administrador");
+                return;
+            }
             this.rolSeleccionado = comboBox1.SelectedValue.ToString();
+            PagoElectronico.MenuPrincipal formMenu = new PagoElectronico.MenuPrincipal(this.rolSeleccionado, this.username);
+            formMenu.ShowDialog();
             this.Close();
         }
         
